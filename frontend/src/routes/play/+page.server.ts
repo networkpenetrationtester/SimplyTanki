128e954bc901518e2e51cf192011edc09bf6d373
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-08-14 14:18:58 -0500

    feat: add UI overlay demo

--- frontend/src/routes/play/+page.server.ts
+++
@@ -1,1 +0,0 @@
-import { THREE } from '$lib';
--- frontend/src/routes/play/+page.svelte
+++ frontend/src/routes/play/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import './page.css';
 	import * as THREE from 'three';
 	import { browser } from '$app/env';
+	import GUIElement from '$lib/components/GUIElement.svelte';
+
+	function range(length: number) {
+		return new Array(length).fill(0).map((_, i) => i);
+	}
 
 	if (browser) {
 		const { innerWidth, innerHeight } = window;
@@ -15,12 +21,16 @@
 		function resize_renderer() {
 			const { innerWidth, innerHeight } = window;
 			renderer.setSize(innerWidth, innerHeight);
+			renderer.setPixelRatio(window.devicePixelRatio);
+			camera.aspect = innerWidth / innerHeight;
+			camera.updateProjectionMatrix();
 		}
 
 		camera.position.z = 1;
 		scene.add(mesh);
 		resize_renderer();
 		renderer.setAnimationLoop(animate);
+		renderer.domElement.style.position = 'absolute';
 
 		document.body.appendChild(renderer.domElement);
 		window.addEventListener('resize', resize_renderer);
@@ -37,12 +47,24 @@
 <svelte:head>
 	<meta charset="utf-8" />
 	<title>My first three.js app</title>
-	<style>
-		body,
-		html {
-			margin: 0;
-			overflow: hidden;
-			background-color: #00000;
-		}
-	</style>
 </svelte:head>
+
+<div class="overlay">
+	<!-- TODO: fix spacing between images -->
+	<!-- TODO: place these in a grid column that doesn't wrap which dynamically resizes horizontally -->
+
+	<GUIElement asset="386" alt="rank" />
+	<GUIElement asset="204" alt="begin_xp" />
+
+	{#each range(40) as i (i)}
+		<GUIElement asset="207" alt="middle" />
+	{/each}
+
+	<GUIElement asset="210" alt="border" />
+
+	{#each range(3) as i (i)}
+		<GUIElement asset="207" alt="middle" />
+	{/each}
+
+	<GUIElement asset="213" alt="end_cry" />
+</div>
