1035f176de32a5f7b7305d16abc55c0b2206e43b
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-08-14 14:20:02 -0500

    feat: create GUIElement component

---
+++ frontend/src/lib/components/GUIElement.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { $GUIElementConfig } from '$lib/types';
+	let props: $GUIElementConfig = $props();
+	const { asset, alt } = $derived(props);
+</script>
+
+<img src={'/resources/' + asset + '.png'} alt={alt ?? 'no alt text'} />
